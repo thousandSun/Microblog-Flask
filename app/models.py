3e7448ee7a404bdb1ed152b139cb163f5543422f
mugiwaraace <filibertoerios@gmail.com>
2020-03-04 11:00:44 -0600

    added migration for the app and different databases for Users and Posts

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):  # SQLAlchemy class model must inherit
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # creates relationship between databases
+    # requires the name of the Class model instead of the database table
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # ForeignKey('table.field') is used to create links between database tables
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -27,6 +27,6 @@ def login():
     # returns true if everything checks out, false if something is missing
     if form.validate_on_submit():
         # shows message confirming credentials, temp solution for logging in users
-        flash(f'Login requested for user {form.username.data}, remember_me {form.remember_me.data}')
+        flash('Login requested for user {}, remember_me {}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
